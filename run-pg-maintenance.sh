00de8470bcf3b3deea1b0dae8d6304b688c838a8
Mike Morano <mmorano@mikeandwan.us>
2020-06-14 07:48:38 -0400

    remove cron hack

--- run-pg-maintenance.sh
+++ run-pg-maintenance.sh
@@ -1,27 +1,16 @@
 #!/bin/bash
 set -e
 
-# poor mans cron
-while true; do
-    sleep 55m
+echo '*** executing postgres maintenance ***'
 
-    echo '*** checking if we need to run postgres maintenance ***'
+echo 'Starting backup for maw_website...'
+pg_dump -h localhost -U postgres -Fc maw_website > /pg_backup/maw_website.`date +\%Y\%m\%d`.dump
 
-    hour=$(date +"%H")
+echo 'Starting backup for idsrv...'
+pg_dump -h localhost -U postgres -Fc idsrv > /pg_backup/idsrv.`date +\%Y\%m\%d`.dump
 
-    if [ "${hour}" = "02" ]; then
-        echo '*** executing postgres maintenance ***'
+echo 'Cleaning old backups...'
+find /pg_backup/* -type f -mtime +5 -not -iname *01.dump -delete
 
-        echo 'Starting backup for maw_website...'
-        pg_dump -h localhost -U postgres -Fc maw_website > /pg_backup/maw_website.`date +\%Y\%m\%d`.dump
-
-        echo 'Starting backup for idsrv...'
-        pg_dump -h localhost -U postgres -Fc idsrv > /pg_backup/idsrv.`date +\%Y\%m\%d`.dump
-
-        echo 'Cleaning old backups...'
-        find /pg_backup/* -type f -mtime +5 -not -iname *01.dump -delete
-
-        echo 'Optimize postgres'
-        vacuumdb -h localhost -U postgres --all --analyze
-    fi
-done
+echo 'Optimize postgres'
+vacuumdb -h localhost -U postgres --all --analyze
